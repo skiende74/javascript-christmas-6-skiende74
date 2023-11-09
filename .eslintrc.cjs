5ab2f719454d8a515054c6490144c721bb2e3452
skiende74 <skiende74@gmail.com>
2023-11-10 04:40:06 +0900

    chore(ESLint, Prettier): ESLint, Prettier 설치 & 설정

---
+++ .eslintrc.cjs
@@ -0,0 +1,30 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: ['airbnb-base','prettier'],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [
+        '.eslintrc.{js,cjs}',
+      ],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  rules: {
+    'max-depth': ['error', 2],
+    'max-params': ['error', 3],
+    'max-lines-per-function': ['error',{max:10}],
+  },
+};
