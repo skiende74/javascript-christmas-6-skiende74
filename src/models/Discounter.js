3c4814fda65a265a6ffacd3632a390239b346126
skiende74 <skiende74@gmail.com>
2023-11-12 15:08:00 +0900

    feat(할인) : 평일, 주말, 특별 할인 기능 추가

--- src/models/Discounter.js
+++ src/models/Discounter.js
@@ -10,7 +10,14 @@ class Discounter {
   #special = 0;
 
   constructor(date) {
-    this.#dday = (date <= 25) * (date + 9) * Conditions.DISCOUNTS.dday;
+    this.#dday = date <= 25 ? (date + 9) * Conditions.DISCOUNTS.dday : 0;
+    this.#weekday =
+      Discounter.#getDayOfWeek(date) <= 4 ? Conditions.DISCOUNTS.weekday : 0;
+    this.#weekend =
+      Discounter.#getDayOfWeek(date) >= 5 ? Conditions.DISCOUNTS.weekend : 0;
+    this.#special = Conditions.SPECIAL_DATES.includes(date)
+      ? Conditions.DISCOUNTS.special
+      : 0;
   }
 
   getDiscounts() {
@@ -27,7 +34,7 @@ class Discounter {
     return sum(Object.values(this.getDiscounts()));
   }
 
-  #getDayOfWeek(date) {
+  static #getDayOfWeek(date) {
     return new Date(`2023-12-${date}`).getDay();
   }
 }
