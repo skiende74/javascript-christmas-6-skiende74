92aae3d6519bc6f904d2fd805883b19397a7529e
skiende74 <skiende74@gmail.com>
2023-11-13 02:48:04 +0900

    feat(Messages): 안내 헤더 Messages.js에 추가.

--- src/constants/Messages.js
+++ src/constants/Messages.js
@@ -4,6 +4,13 @@ const Messages = Object.freeze({
     '12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)',
   INPUT_ORDER:
     '주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)',
+
+  ORDER_MENU: '<주문 메뉴>',
+  TOTAL_PRICE_BEFORE_DISCOUNT: '<할인 전 총주문 금액>',
+  BENEFIT_LIST: '<혜택 내역>',
+  TOTAL_BENEFIT_PRICE: '<총혜택 금액>',
+  EXPECTED_PURCHASE_AFTER_DISCOUNT: '<할인 후 예상 결제 금액>',
+  EVENT_BADGE: '<12월 이벤트 배지>',
 });
 
 export default Messages;
