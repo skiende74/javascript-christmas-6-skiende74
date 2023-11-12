5a3431ab0ae239ca89254635eac4472b7faa324e
skiende74 <skiende74@gmail.com>
2023-11-12 13:26:36 +0900

    test(주문): 테스트코드 버그픽스
    - toThrow()가 내부에 물려있던 것을 외부로 뺌.

--- __tests__/Order.test.js
+++ __tests__/Order.test.js
@@ -1,16 +1,19 @@
 import Order from '../src/models/Order';
 
+// eslint-disable-next-line max-lines-per-function
 describe('주문', () => {
-  test.each(['시저샐러드-1', '시저샐러드-1,티본스테이크-2,바비큐립-1'])(
+  test.each(['샴페인-1', '샴페인-1,제로콜라-2,바비큐립-1'])(
     '올바른 입력',
     (order) => {
       expect(() => new Order(order)).not.toThrow();
     },
   );
+
   test.each(['감자-1', '시저샐러드-2,튤립-1'])('없는 메뉴 입력', (order) => {
-    expect(() => new Order(order).toThrow());
+    expect(() => new Order(order)).toThrow();
   });
-  test.each(['시저샐러드-0'])('잘못된 갯수 입력', (order) => {
-    expect(() => new Order(order).toThrow());
+
+  test.each(['샴페인-0'])('잘못된 갯수 입력', (order) => {
+    expect(() => new Order(order)).toThrow();
   });
 });
