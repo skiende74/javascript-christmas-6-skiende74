f299e3726c3e625318e5ef7108fbd4dd56d11c58
skiende74 <skiende74@gmail.com>
2023-11-13 02:53:08 +0900

    feat(OutputView): 헤더문구 출력함수 추가

--- src/views/OutputView.js
+++ src/views/OutputView.js
@@ -1,9 +1,21 @@
 import { Console } from '@woowacourse/mission-utils';
+import Messages from '../constants/Messages';
 
 const OutputView = {
   printMenu() {
-    Console.print('<주문 메뉴>');
-    // ...
+    Console.print(Messages.ORDER_MENU);
+  },
+  printPriceBeforeDiscount() {
+    Console.print(Messages.TOTAL_PRICE_BEFORE_DISCOUNT);
+  },
+  printBenfitList() {
+    Console.print(Messages.BENEFIT_LIST);
+  },
+  printBenfitPrice() {
+    Console.print(Messages.TOTAL_BENEFIT_PRICE);
+  },
+  printExpectedPurchase() {
+    Console.print(Messages.EXPECTED_PURCHASE_AFTER_DISCOUNT);
   },
 
   print(message) {
