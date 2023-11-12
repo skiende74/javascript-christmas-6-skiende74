66fff5bede5781ade870ca90f4d56d5c099511b6
skiende74 <skiende74@gmail.com>
2023-11-12 15:05:21 +0900

    test(할인): 평일,주말,특별 할인 테스트코드 추가

--- __tests__/Discounter.test.js
+++ __tests__/Discounter.test.js
@@ -1,5 +1,7 @@
 import Discounter from '../src/models/Discounter';
+import Condition from '../src/constants/Conditions';
 
+// eslint-disable-next-line max-lines-per-function
 describe('할인', () => {
   test.each([
     [3, 1200],
@@ -9,6 +11,42 @@ describe('할인', () => {
   ])('D-Day 할인', (date, expectedDiscount) => {
     // when
     const discount = new Discounter(date).getDiscounts().dday;
+    // then
+    expect(discount).toBe(expectedDiscount);
+  });
+
+  test.each([
+    [2, 0],
+    [3, 2023],
+    [7, 2023],
+    [9, 0],
+  ])('평일 할인', (date, expectedDiscount) => {
+    // when
+    const discount = new Discounter(date).getDiscounts().weekday;
+    // then
+    expect(discount).toBe(expectedDiscount);
+  });
+
+  test.each([
+    [1, 2023],
+    [2, 2023],
+    [8, 2023],
+    [10, 0],
+  ])('주말 할인', (date, expectedDiscount) => {
+    // when
+    const discount = new Discounter(date).getDiscounts().weekend;
+    // then
+    expect(discount).toBe(expectedDiscount);
+  });
+
+  test.each([
+    [3, 1000],
+    [10, 1000],
+    [25, 1000],
+    [23, 0],
+  ])('특별 할인', (date, expectedDiscount) => {
+    // when
+    const discount = new Discounter(date).getDiscounts().special;
     // then
     expect(discount).toBe(expectedDiscount);
   });
