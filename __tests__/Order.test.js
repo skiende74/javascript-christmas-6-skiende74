73abb34ab6c1cb2a8e2473c7c56ba694600115a2
skiende74 <skiende74@gmail.com>
2023-11-12 13:56:12 +0900

    test(주문): 음료만 주문 불가 테스트코드 추가

--- __tests__/Order.test.js
+++ __tests__/Order.test.js
@@ -10,17 +10,21 @@ describe('주문', () => {
   );
 
   test.each(['감자-1', '시저샐러드-2,튤립-1'])('없는 메뉴 입력', (order) => {
-    expect(() => new Order(order)).toThrow();
+    expect(() => new Order(order)).toThrow('[ERROR]');
   });
 
-  test.each(['샴페인-0'])('잘못된 갯수 입력', (order) => {
-    expect(() => new Order(order)).toThrow();
+  test.each(['티본스테이크-0'])('잘못된 갯수 입력', (order) => {
+    expect(() => new Order(order)).toThrow('[ERROR]');
   });
 
   test.each(['샴페인-1,샴페인-2', '샴페인-1,레드와인-2,샴페인-2'])(
     '중복메뉴 입력',
     (order) => {
-      expect(() => new Order(order)).toThrow();
+      expect(() => new Order(order)).toThrow('[ERROR]');
     },
   );
+
+  test.each(['샴페인-1,레드와인-2', '샴페인-4'])('음료만 주문불가', (order) => {
+    expect(() => new Order(order)).toThrow('[ERROR]');
+  });
 });
