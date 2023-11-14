8b4c144296ebca23c63d79e0544eae5aac63d628
skiende74 <skiende74@gmail.com>
2023-11-14 11:15:29 +0900

    refactor(입력): robust 입력책임을 메인컨트롤러->InputView
    - MVC에서 View는 모델에 의존할 수 있기 때문에,
    - 올바른 입력이 왔는지 판단로직은 Input에서 처리하는게 맞다고 판단.

--- src/views/InputView.js
+++ src/views/InputView.js
@@ -1,12 +1,38 @@
+/* eslint-disable no-await-in-loop */
+/* eslint-disable no-constant-condition */
 import { Console } from '@woowacourse/mission-utils';
 import Messages from '../constants/Messages.js';
+import VisitDate from '../domains/VisitDate.js';
+import Order from '../domains/Order.js';
+import OutputView from './OutputView.js';
 
-const InputView = {
+const Private = {
   async readDate() {
     return Console.readLineAsync(`${Messages.VISIT_DATE}\n`);
   },
   async readOrder() {
     return Console.readLineAsync(`${Messages.INPUT_ORDER}\n`);
+  },
+
+  async robustInput(readline, Object) {
+    while (true) {
+      try {
+        const input = await readline();
+        return new Object(input);
+      } catch (e) {
+        OutputView.print(e.message);
+      }
+    }
+  },
+};
+
+const InputView = {
+  async readVisitDate() {
+    return Private.robustInput(Private.readDate, VisitDate);
+  },
+
+  async readOrder() {
+    return Private.robustInput(Private.readOrder, Order);
   },
 };
 
